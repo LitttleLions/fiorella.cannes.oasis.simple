0d2ffa73daae178643c96f0bdf94f2301b7bbd48
LitttleLions <sirtis.fran@gmail.com>
2026-03-18 11:37:42 +0100

    new english

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import Impressum from './pages/Impressum';
 import Datenschutz from './pages/Datenschutz';
+import { LanguageProvider } from './i18n/LanguageContext';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="impressum" element={<Impressum />} />
-          <Route path="datenschutz" element={<Datenschutz />} />
-        </Route>
-      </Routes>
-    </Router>
+    <LanguageProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="impressum" element={<Impressum />} />
+            <Route path="datenschutz" element={<Datenschutz />} />
+          </Route>
+        </Routes>
+      </Router>
+    </LanguageProvider>
   );
 }
